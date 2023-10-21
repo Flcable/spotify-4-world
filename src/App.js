16584cc33fabdd9356c6c4a0e0529fd48db17883
Flcable <flcable@gmail.com>
2023-10-21 20:29:35 +0100

    Version Fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import NavigationBar from './components/NavigationBar/NavigationBar';
@@ -8,11 +9,31 @@ import Charger from './components/Charger';
 
 const App = () => {
   return (
-    <div className="App">
-      {  /*<img src={logo} className='App-logo' alt='logo'/> */}
+
+
+    <Router>
       <NavigationBar />
-      <Charger />
-    </div>
+
+      <Switch>
+
+        <Route path="/profile">
+          <h1>Welcome !!!</h1>
+        </Route>
+
+        <Route path="/">
+
+          <div className="App">
+            {  /*<img src={logo} className='App-logo' alt='logo'/> */}
+
+            <Charger />
+          </div>
+
+        </Route>
+
+      </Switch>
+
+    </Router>
+
   );
 };
 
